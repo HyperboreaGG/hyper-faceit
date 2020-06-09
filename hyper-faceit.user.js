6eeeb5e994333a1d989eaaa4dd39f9a6614495a0
Michael Khaimov <khaimovmr@gmail.com>
2020-06-09 14:35:11 +0300

    fix bugs

--- hyper-faceit.user.js
+++ hyper-faceit.user.js
@@ -1,40 +1,25 @@
 // ==UserScript==
 // @name         Hyper FaceIt
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.faceit.com/*
 // @grant        none
-// @require      https://cdn.jsdelivr.net/gh/skepticfx/wshook@0.1.1/wsHook.js
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js
+// @require      https://cdn.jsdelivr.net/gh/khaimovmr/wshook@0.1.2/wsHook.js
+// @require      https://cdn.jsdelivr.net/gh/hyperboreagg/hyper-faceit-assets@0.3.1/wshook-init.js
 // ==/UserScript==
-
-// Make sure your program calls `wsClient.onmessage` event handler somewhere.
-wsHook.after = function(messageEvent, url, wsObject) {
-    var bodyStart = messageEvent.data.search('\<body\>');
-    var bodyEnd = messageEvent.data.search('\<\/body\>');
-
-    if (bodyStart > -1 && bodyEnd > -1) {
-        let message = messageEvent.data.slice(bodyStart + 6, bodyEnd);
-        console.log(message);
-    }
-
-    return messageEvent;
-}
-
-// if you do not want to propagate the MessageEvent further down, just return null
-wsHook.after = function(messageEvent, url, wsObject) {
-    //console.log("3Received message from " + url + " : " + messageEvent.data);
-    // This example can ping-pong forever, so maybe use some conditions
-    wsObject.send("Intercepted and sent again")
-    return null;
-}
 
 (function() {
     'use strict';
 
     $(document).ready(function() {
-        $('head').append('<style>.sc-jjOAeO.hbbTPq:hover {cursor: pointer;}</style>');
+        let styleElement = document.createElement('style');
+        styleElement.innerHTML = '.sc-jjOAeO.hbbTPq:hover {cursor: pointer;}';
+        styleElement.type = 'text/css';
+        document.head.insertBefore(styleElement, document.head.firstChild);
+        //$('head').append('<style>.sc-jjOAeO.hbbTPq:hover {cursor: pointer;}</style>');
         $(document).on('click', '.sc-jjOAeO.hbbTPq', function(){
             var parent = $(this).parent();
             parent.parent().find('input[type=radio]').prop('checked', false);
@@ -42,4 +27,3 @@ wsHook.after = function(messageEvent, url, wsObject) {
         });
     });
 })();
-
